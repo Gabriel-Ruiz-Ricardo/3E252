c56c112c96fbbfad0af8692bc101989d570e36e2
Gabriel-Ruiz-Ricardo <a24310399@live.ceti.mx>
2025-10-09 02:02:03 +0000

    Practica 3

--- src/punteros.cpp
+++ src/punteros.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
@@ -6,8 +7,18 @@ int main(int argc, char const *argv[])
     pNumero = new int;
     *pNumero = 25;
 
-    std::cout <<"Direccion: " << pNumero << '\n';
-    std::cout <<"Valor: " << *pNumero << '\n';
-    std::cout <<"Tamaño: " << sizeof(pNumero) << '\n';
+    cout <<"Direccion: " << pNumero << '\n';
+    cout <<"Valor: " << *pNumero << '\n';
+    cout <<"Tamaño: " << sizeof(pNumero) << '\n';
+
+    cout<< "Tipos de datos: " << endl;
+
+    cout<< "Tamaño int : " << sizeof(int) << endl;
+    cout<< "Tamaño long : " << sizeof(long) << endl;
+    cout<< "Tamaño float : " << sizeof(float) << endl;
+    cout<< "Tamaño double : " << sizeof(double) << endl;
+    cout<< "Tamaño char : " << sizeof(char) << endl;
+    cout<< "Tamaño bool : " << sizeof(bool) << endl;
+    cout<< "Tamaño byte : " << sizeof(byte) << endl;
     return 0;
 }
